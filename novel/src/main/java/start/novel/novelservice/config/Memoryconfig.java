c1b29f7d3361e34f7acd00c99a4f10366a13165d
kim neko <kds98611275@gmail.com>
2023-06-13 23:41:50 +0900

    Commit

--- novel/src/main/java/start/novel/novelservice/config/Memoryconfig.java
+++ novel/src/main/java/start/novel/novelservice/config/Memoryconfig.java
@@ -3,9 +3,13 @@ package start.novel.novelservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import start.novel.novelservice.memory.MemoryNovelDataRepository;
+import start.novel.novelservice.memory.MemoryUserRepository;
 import start.novel.novelservice.repository.NovelDataRepository;
+import start.novel.novelservice.repository.UserRepository;
 import start.novel.novelservice.service.NovelService;
 import start.novel.novelservice.service.NovelServiceV1;
+import start.novel.novelservice.service.UserService;
+import start.novel.novelservice.service.UserServiceV3;
 
 @Configuration
 public class Memoryconfig {
@@ -18,5 +22,15 @@ public class Memoryconfig {
     @Bean
     public NovelDataRepository novelRepository() {
         return new MemoryNovelDataRepository();
+    }
+
+    @Bean
+    UserService userService() {
+        return new UserServiceV3(userRepository());
+    }
+
+    @Bean
+    UserRepository userRepository() {
+        return new MemoryUserRepository();
     }
 }
